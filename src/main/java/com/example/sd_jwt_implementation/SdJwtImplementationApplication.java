467af0e75a899319a922590c23e2c0abb0a11172
Awambeng Rodrick <awambengrodrick@gmail.com>
2024-09-18 16:42:34 +0100

    added project setup

---
+++ src/main/java/com/example/sd_jwt_implementation/SdJwtImplementationApplication.java
@@ -0,0 +1,13 @@
+package com.example.sd_jwt_implementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdJwtImplementationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdJwtImplementationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sd_jwt_implementation/SdJwtImplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sd_jwt_implementation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdJwtImplementationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
